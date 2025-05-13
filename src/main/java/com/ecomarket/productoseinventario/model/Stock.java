0ddd5f09e849e5e1f5ff9c11c394e85c81adc6db
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-05-12 21:45:34 -0400

    Test

--- src/main/java/com/ecomarket/productoseinventario/model/Stock.java
+++ src/main/java/com/ecomarket/productoseinventario/model/Stock.java
@@ -5,35 +5,24 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 
 @Entity
-@Table(name = "producto")
+@Table(name = "stock")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Producto {
+public class Stock {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
-    private String nombre;
+    private Integer cantidad;
 
     @Column(nullable = false)
-    private String descripcion;
-
-    @Column(nullable = false)
-    private Integer precio;
-
-    // Implementar conexiones (Categoria, Stock)
-    @OneToOne
-    @JoinTable(name = "stock")
-    private Stock stock;
-
-    @ManyToOne
-    @JoinTable(name = "categoria")
-    private Categoria categoria;
-
+    private Date fecha_actualizacion;
 
 }
