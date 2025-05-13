49fca2ab5d613b925ee9ed917ed90a65efe160a3
Sillita <be.lizamac@duocuc.cl>
2025-05-12 20:24:37 -0400

    Estructura principal del proyecto
    Agregue la estructura del proyecto

---
+++ EcoMarket/src/main/java/com/ecomarket/productoseinventario/ProductoseinventarioApplication.java
@@ -0,0 +1,13 @@
+package com.ecomarket.productoseinventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductoseinventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductoseinventarioApplication.class, args);
+	}
+
+}
---
+++ EcoMarket/src/test/java/com/ecomarket/productoseinventario/ProductoseinventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomarket.productoseinventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductoseinventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
