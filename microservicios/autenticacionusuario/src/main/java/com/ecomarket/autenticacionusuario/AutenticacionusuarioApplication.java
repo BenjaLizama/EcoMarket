e17e7435b6f411d4abe56fd6c8e0e89fff09c127
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-05-15 15:18:49 -0400

    Todo funcionando bien.

--- microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/AutenticacionusuarioApplication.java
+++ microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/AutenticacionusuarioApplication.java
@@ -3,7 +3,7 @@ package com.ecomarket.autenticacionusuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@SpringBootApplication
 public class AutenticacionusuarioApplication {
 
 	public static void main(String[] args) {
