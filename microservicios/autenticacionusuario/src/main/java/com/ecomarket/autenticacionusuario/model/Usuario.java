45b73a4371941ff0ccc4fe39b1560d154bed4265
n3k0k4w4 <luccianomartines@gmail.com>
2025-05-15 17:56:52 -0400

    agrege clase Usuario

---
+++ microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.ecomarket.autenticacionusuario.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Usuario {
+
+
+}
