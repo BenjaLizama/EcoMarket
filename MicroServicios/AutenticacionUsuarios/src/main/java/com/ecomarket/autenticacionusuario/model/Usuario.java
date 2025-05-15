cec20e1dd5b90f4c58a5895dd111aa5a88383fcf
nekokawa <lucc.martinez@duocuc.cl>
2025-05-15 14:22:21 -0400

    agregue carpetas del microservicio autenticacionusuario y cree la clase Usuario

---
+++ MicroServicios/AutenticacionUsuarios/src/main/java/com/ecomarket/autenticacionusuario/model/Usuario.java
@@ -0,0 +1,15 @@
+package com.ecomarket.autenticacionusuario.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    private String nombre;
+    private String correo;
+
+}
