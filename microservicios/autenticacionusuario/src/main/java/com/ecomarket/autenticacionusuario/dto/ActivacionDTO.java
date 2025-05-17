ea8fc2877cbba7a59d0157cc130c4dc8f408f900
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-05-17 18:33:40 -0400

    Agregue LoginDTO y ActivacionDTO.

---
+++ microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/dto/ActivacionDTO.java
@@ -0,0 +1,16 @@
+package com.ecomarket.autenticacionusuario.dto;
+
+
+public class ActivacionDTO {
+
+    private Boolean estado;
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+}
---
+++ microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/dto/LoginDTO.java
@@ -0,0 +1,16 @@
+package com.ecomarket.autenticacionusuario.dto;
+
+public class LoginDTO {
+
+    private String correo;
+    private String clave;
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+}
