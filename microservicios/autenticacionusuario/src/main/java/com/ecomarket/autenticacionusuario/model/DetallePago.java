2836b292b14cd679cbef2c340eee7fcfdeebd698
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-05-17 12:01:03 -0400

    Implemente entidad DetallePago.

---
+++ microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/model/DetallePago.java
@@ -0,0 +1,19 @@
+package com.ecomarket.autenticacionusuario.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "detalle_pago")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetallePago {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetallePago;
+
+}
