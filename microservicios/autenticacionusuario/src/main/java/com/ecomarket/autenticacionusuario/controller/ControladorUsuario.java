d5c661099d533561bac2e67621310a259e1c23b2
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-05-17 13:03:36 -0400

    elimine importacion que no se estaba usando.

--- microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/controller/ControladorUsuario.java
+++ microservicios/autenticacionusuario/src/main/java/com/ecomarket/autenticacionusuario/controller/ControladorUsuario.java
@@ -6,7 +6,6 @@ import com.ecomarket.autenticacionusuario.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.handler.UserRoleAuthorizationInterceptor;
 
 import java.util.List;
 
