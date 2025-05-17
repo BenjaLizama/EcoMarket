31484f8997f7a211576b1cf0de4bace421f0d57a
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-05-16 23:09:48 -0400

    Implemente nuevamente metodo para llamar productos disponibles.

--- microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/repository/ProductoRepository.java
+++ microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/repository/ProductoRepository.java
@@ -20,7 +20,7 @@ public interface ProductoRepository extends JpaRepository<Producto, Long> {
    - count()
     */
 
-    @Query(value = "SELECT * FROM producto p LEFT OUTER JOIN stock s ON p.stock_id = s.id WHERE s.cantidad > 0;", nativeQuery = true)
+    @Query("SELECT p FROM Producto p WHERE p.stock.cantidad > 0")
     List<Producto> buscarProductosDisponibles();
 
 }
