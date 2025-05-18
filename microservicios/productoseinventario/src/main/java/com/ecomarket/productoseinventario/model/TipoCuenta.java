2e9dfa5f886d134cb0571df003895365b9059069
Benjamin Lizama <be.lizamac@duocuc.cl>
2025-05-17 21:41:45 -0400

    Implemente Entidad TipoCuenta.

---
+++ microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/model/TipoCuenta.java
@@ -0,0 +1,22 @@
+package com.ecomarket.productoseinventario.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tipo_cuenta")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TipoCuenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idTipoCuenta;
+
+    @Column(nullable = false)
+    private String nombreTipoCuenta;
+
+}
