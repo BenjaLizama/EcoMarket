1f380afcc7f3dc635f40859ef775194d3507d300
nekokawa <luccianomartines@gmail.com>
2025-06-19 18:40:28 -0400

    ESTABA IMPLEMENTANDO COMO AGREGARLE UNA CATEGORIA A LOS PRODUTOS

--- microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/DataLoader.java
+++ microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/DataLoader.java
@@ -1,7 +1,9 @@
 package com.ecomarket.productoseinventario;
 
 
+import com.ecomarket.productoseinventario.model.Categoria;
 import com.ecomarket.productoseinventario.model.Producto;
+import com.ecomarket.productoseinventario.repository.CategoriaRepository;
 import com.ecomarket.productoseinventario.repository.ProductoRepository;
 import com.ecomarket.productoseinventario.services.CategoriaService;
 import net.datafaker.Faker;
@@ -20,19 +22,34 @@ public class DataLoader implements CommandLineRunner {
     @Autowired
     private ProductoRepository productoRepository;
     @Autowired
+    private CategoriaRepository categoriaRepository;
+    @Autowired
     private CategoriaService categoriaService;
     @Override
 
     public void run(String... args) throws Exception {
         Faker faker = new Faker();
         Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            Categoria nuevacategoria = new Categoria();
+            nuevacategoria.setNombreCategoria(faker.lorem().toString());
+            categoriaRepository.save(nuevacategoria);
+
+
+        }
+
         for (int i = 0; i < 10; i++) {
             Producto nuevoproducto = new Producto();
-
+            // IMPLEMENTARR COMO AGREGARLE LA CATEGORIA A EL PRODUCTO
+            //Long idrandom = faker.number().numberBetween(1L,5L);
+            //nuevoproducto.setCategoria(categoriaService.findById(idrandom));
             nuevoproducto.setNombreProducto(faker.commerce().productName());
             nuevoproducto.setPrecio(faker.number().numberBetween(1000, 20000));
             nuevoproducto.setDescripcion(faker.lorem().sentence());
+
+
             productoRepository.save(nuevoproducto);
+
 
 
         }
