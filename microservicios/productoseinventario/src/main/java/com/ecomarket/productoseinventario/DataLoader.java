55375f4586415a853a8bbf8a7a247baf68ea6b9d
nekokawa <lucc.martinez@duocuc.cl>
2025-06-25 23:15:33 -0400

    implemente datafaker en productos inventario creando stock y agregandoselo al  producto

--- microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/DataLoader.java
+++ microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/DataLoader.java
@@ -3,9 +3,12 @@ package com.ecomarket.productoseinventario;
 
 import com.ecomarket.productoseinventario.model.Categoria;
 import com.ecomarket.productoseinventario.model.Producto;
+import com.ecomarket.productoseinventario.model.Stock;
 import com.ecomarket.productoseinventario.repository.CategoriaRepository;
 import com.ecomarket.productoseinventario.repository.ProductoRepository;
 import com.ecomarket.productoseinventario.services.CategoriaService;
+import com.ecomarket.productoseinventario.services.ProductoService;
+import com.ecomarket.productoseinventario.services.StockService;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +16,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import net.datafaker.Faker;
 
+
+import java.time.LocalDateTime;
 import java.util.Random;
 
 @Profile("dev")
@@ -25,31 +30,34 @@ public class DataLoader implements CommandLineRunner {
     private CategoriaRepository categoriaRepository;
     @Autowired
     private CategoriaService categoriaService;
+    @Autowired
+    private StockService stockService;
+    @Autowired
+    private ProductoService productoService;
     @Override
 
     public void run(String... args) throws Exception {
         Faker faker = new Faker();
         Random random = new Random();
-        /*
-        for (int i = 0; i < 5; i++) {
-            Categoria nuevacategoria = new Categoria();
-            nuevacategoria.setNombreCategoria(faker.lorem().toString());
-            categoriaRepository.save(nuevacategoria);
-
-
-        }*/
 
         for (int i = 0; i < 10; i++) {
+            Stock nuevoStock = new Stock();
+            nuevoStock.setCantidad(faker.number().numberBetween(10, 100));
+            nuevoStock.setFecha_actualizacion(LocalDateTime.now());
+            stockService.save(nuevoStock);
+
             Producto nuevoproducto = new Producto();
-            // IMPLEMENTARR COMO AGREGARLE LA CATEGORIA A EL PRODUCTO
-            //Long idrandom = faker.number().numberBetween(1L,5L);
-            //nuevoproducto.setCategoria(categoriaService.findById(idrandom));
             nuevoproducto.setNombreProducto(faker.commerce().productName());
             nuevoproducto.setPrecio(faker.number().numberBetween(1000, 20000));
             nuevoproducto.setDescripcion(faker.lorem().sentence());
 
 
-            productoRepository.save(nuevoproducto);
+            productoService.save(nuevoproducto);
+
+            nuevoproducto.setStock(nuevoStock);
+
+            nuevoproducto.setCategoria(categoriaService.findById(faker.number().numberBetween(20L,22L)).get());
+            productoService.save(nuevoproducto);
 
 
 
