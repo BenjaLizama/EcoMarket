9ff54e46ca8c75e20bad3c0f4223613b3861114b
martin lorca <ma.lorcam@duocuc.cl>
2025-06-26 21:50:44 -0400

    ANOTACIONES CARRITO

--- microservicios/carro/src/main/java/com/ecomarket/carrito/controller/CarritoController.java
+++ microservicios/carro/src/main/java/com/ecomarket/carrito/controller/CarritoController.java
@@ -6,6 +6,13 @@ import com.ecomarket.carrito.model.ProductoMSDTO;
 import com.ecomarket.carrito.services.CarritoService;
 import com.ecomarket.carrito.services.ProductoMSService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/carrito")
-@Tag(name = "Carrito", description =" operaciones relacionadas al Carrito de compras")
+@Tag(name = "Carrito", description = "Operaciones relacionadas con la gestión del carrito y sus productos")
 public class CarritoController {
 
     @Autowired
@@ -26,8 +33,40 @@ public class CarritoController {
 
     // Crear carrito para usuario => Segun ID
     @PostMapping("/crear/{idUsuario}")
-    @Operation(summary = "crear Carrito para usuario segun ID", description = "crea Carrito para el usuario segun el ID")
+    @Operation(
+            summary = "Crear carrito para un usuario",
+            description = "Crea un nuevo carrito de compras vacío para el usuario especificado por su ID.",
+            parameters = {
+                    @Parameter(name = "idUsuario", description = "ID del usuario que recibe el carrito", example = "5")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Carrito creado correctamente",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = Carrito.class),
+                            examples = @ExampleObject(
+                                    name = "",
+                                    summary = "",
+                                    value = """
+                        {
+                          "id": 1,
+                          "usuarioId": 5,
+                          "productos": [],
+                          "total": 0.0
+                        }
+                    """
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "ID invalido o error de validacion")
+    })
+
     public ResponseEntity<Carrito> crear(@PathVariable Long idUsuario) {
+
+
         try {
             Carrito nuevoCarrito = carritoService.crearCarrito(idUsuario);
             return ResponseEntity.ok(nuevoCarrito);
@@ -39,8 +78,39 @@ public class CarritoController {
 
     // Obtener productos dentro del carrito.
     @GetMapping("/productos/{idUsuario}")
-    @Tag(name = "productos", description =" operacion relacionada producto del carrito")
-    @Operation(summary = "obtener producto", description = "obtener uno o varios productos dentro del carrito")
+    @Operation(
+            summary = "Obtener productos del carrito de un usuario",
+            description = "Retorna una lista de ítems (productos) que están actualmente en el carrito del usuario.",
+            parameters = {
+                    @Parameter(name = "idUsuario", description = "ID del usuario", example = "5")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Lista de productos obtenida correctamente",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = Item.class)),
+                            examples = @ExampleObject(
+                                    name = "ItemsCarrito",
+                                    summary = "Ejemplo de ítems en el carrito",
+                                    value = """
+                        [
+                          {
+                            "productoId": 1,
+                            "nombre": "xxxx",
+                            "cantidad":xx ,
+                            "precioUnitario": xxxxx,
+                            "subtotal":xxxxx 
+                          }
+                        ]
+                    """
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "ID inválido")
+    })
     public ResponseEntity<List<Item>> obtenerProductosEnCarrito(@PathVariable Long idUsuario) {
         try {
             List<Item> itemList = carritoService.itemsCarrito(idUsuario);
@@ -53,6 +123,35 @@ public class CarritoController {
 
     // Obtener productos
     @GetMapping("/productos")
+    @Operation(
+            summary = "Obtener todos los productos disponibles",
+            description = "Obtiene todos los productos disponibles desde el microservicio de productos."
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Lista de productos obtenida",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = ProductoMSDTO.class)),
+                            examples = @ExampleObject(
+                                    name = "ProductosEjemplo",
+                                    summary = "Ejemplo de productos disponibles",
+                                    value = """
+                        [
+                          {
+                            "id": 1,
+                            "nombre": "xxx",
+                            "descripcion": "",
+                            "precio": xxxxx 
+                          }
+                        ]
+                    """
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "400", description = "Error al obtener productos")
+    })
     public ResponseEntity<List<ProductoMSDTO>> obtenerProductos() {
         try {
             List<ProductoMSDTO> productos = productoMSService.obtenerProductos();
@@ -65,6 +164,22 @@ public class CarritoController {
 
     // Agregar producto al carrito
     @PutMapping("/productos/{idUsuario}/agregar/{idProducto}")
+    @Operation(
+            summary = "Agregar producto al carrito",
+            description = "Agrega un producto al carrito del usuario especificado.",
+            parameters = {
+                    @Parameter(name = "idUsuario", description = "ID del usuario", example = "5"),
+                    @Parameter(name = "idProducto", description = "ID del producto a agregar", example = "101")
+            }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Producto agregado correctamente al carrito",
+                    content = @Content(schema = @Schema(implementation = Carrito.class))
+            ),
+            @ApiResponse(responseCode = "400", description = "Error al agregar producto")
+    })
     public ResponseEntity<Carrito> agregarProducto(@PathVariable Long idUsuario, @PathVariable Long idProducto) {
         try {
             Carrito carrito = productoMSService.agregarProducto(idUsuario, idProducto);
