c78840e4f0061ac2f621a3a823bc55a3b53114fa
nekokawa <lucc.martinez@duocuc.cl>
2025-07-12 18:30:00 -0400

    implemente HATE OAS

--- microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/assembler/ProductoAssembler.java
+++ microservicios/productoseinventario/src/main/java/com/ecomarket/productoseinventario/assembler/ProductoAssembler.java
@@ -6,18 +6,23 @@ import com.ecomarket.productoseinventario.model.Producto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
+@Component
 public class ProductoAssembler implements RepresentationModelAssembler<Producto,EntityModel<Producto>> {
-    @Autowired
-    private ActualizarProductoDTO actualizarProductoDTO;
     @Override
     public EntityModel<Producto> toModel(Producto producto){
         return EntityModel.of(producto,
-                linkTo(methodOn(ProductoController.class).obtenerProducto(producto.getIdProducto())).withRel("aobtener productos"),
+                linkTo(methodOn(ProductoController.class).obtenerProducto(producto.getIdProducto())).withRel("obtener productos"),
+
                 linkTo(methodOn(ProductoController.class).eliminarProducto(producto.getIdProducto())).withRel("elimar producto"),
-                linkTo(methodOn(ProductoController.class).actualizarProducto(producto.getIdProducto(),null)).withRel("actualizar productos")
+
+                linkTo(methodOn(ProductoController.class).actualizarProducto(producto.getIdProducto(),null)).withRel("actualizar productos"),
+
+                linkTo(methodOn(ProductoController.class).actualizarCategoriaProducto(producto.getIdProducto(), null)).withRel("actualizar categoria"),
+
+                linkTo(methodOn(ProductoController.class).actualizarStockProducto(producto.getIdProducto(),null)).withRel("actualizar stock del producto")
 
 
 
